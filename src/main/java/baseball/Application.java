9373e6bbded3b81e508ab2a1a6f58593469e9375
KWAK-JINHO <kjhkjhkjh1012@gmail.com>
2024-07-28 23:59:43 +0900

    feat(Application): 3개의 숫자를 모두 맞췄을 때 게임 종료 문구 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -22,39 +22,48 @@ public class Application {
         }
          System.out.println("컴퓨터가 생성한 숫자: " + computer);
 
-        // 사용자에게 서로다른 3자리 수 입력 받기
-        System.out.print("숫자를 입력해주세요 : ");
-        String readInput = Console.readLine();
-        //System.out.println(readInput);
-        //System.out.println(readInput.getClass().getName());
+        boolean gameRunning = true;
+        while (gameRunning) {
+            // 사용자에게 서로다른 3자리 수 입력 받기
+            System.out.print("숫자를 입력해주세요 : ");
+            String readInput = Console.readLine();
+            //System.out.println(readInput);
+            //System.out.println(readInput.getClass().getName());
 
-        // 힌트 출력 기능 (1.글자 한개씩 관리해서 한자리씩 비교)
-        String[] parts = readInput.split(""); // 입력값을 개별적으로 처리하기 위해서 split 으로 분리
-        // 1. 스트라이크 출력 기능
-        int strikes = 0;
-        for (int i = 0; i < 3; i++) {
-            int userNumber = Integer.parseInt(parts[i]); // 사용자 입력값에서 숫자 가져오기
-            if (computer.get(i) == userNumber) {
-                strikes++;
+            // 힌트 출력 기능 (1.글자 한개씩 관리해서 한자리씩 비교)
+            String[] parts = readInput.split(""); // 입력값을 개별적으로 처리하기 위해서 split 으로 분리
+            // 1. 스트라이크 출력 기능
+            int strikes = 0;
+            for (int i = 0; i < 3; i++) {
+                int userNumber = Integer.parseInt(parts[i]); // 사용자 입력값에서 숫자 가져오기
+                if (computer.get(i) == userNumber) {
+                    strikes++;
+                }
             }
-        }
-        // 스트라이크 결과 출력
-        System.out.println(strikes + " 스트라이크");
+            // 스트라이크 결과 출력
+            System.out.println(strikes + " 스트라이크");
 
-        // 2. 볼 출력 기능
-        int balls = 0;
-        for (int i = 0; i < 3; i++) {
-            int userNumber = Integer.parseInt(parts[i]);
-            if (computer.contains(userNumber) && computer.get(i) != userNumber) {
-                balls++;
+            // 2. 볼 출력 기능
+            int balls = 0;
+            for (int i = 0; i < 3; i++) {
+                int userNumber = Integer.parseInt(parts[i]);
+                if (computer.contains(userNumber) && computer.get(i) != userNumber) {
+                    balls++;
+                }
             }
-        }
-        // 볼 결과 출력
-        System.out.println(balls + " 볼");
+            // 볼 결과 출력
+            System.out.println(balls + " 볼");
 
-        // 3. 낫싱 출력 기능
-        if (strikes == 0 && balls == 0) {
-            System.out.println("낫싱");
+            // 3. 낫싱 출력 기능
+            if (strikes == 0 && balls == 0) {
+                System.out.println("낫싱");
+            }
+
+            // 4. 3스트라이크 일때, 게임 종료 문구 출력
+            if (strikes == 3) {
+                System.out.println("게임 종료! 당신이 이겼습니다.");
+                gameRunning = false; // 게임 종료
+            }
         }
     }
 }
